5c7ea695453e94c8e8fac1f710ea2436fbecce9d
Jose Luis Rego <regorodrijl@gmail.com>
2018-02-01 21:31:53 +0100

    lipieza archivos

--- index.php
+++ index.php
@@ -11,41 +11,14 @@ error_reporting(E_ALL);
   <link rel="icon" href="../../favicon.ico">
   <link rel="stylesheet" type="text/css" href="css/ClaustroiNet.css">
   <title>ClaustroiNet</title>
-
-  <!-- jQuery CDN versión 2.2.4 ya que bootstrap no sopeorta la 3 -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
     crossorigin="anonymous">
-
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
     crossorigin="anonymous">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-    crossorigin="anonymous"></script>
-  <script>
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-86031054-1', 'auto');
-    ga('send', 'pageview');
-  </script>
-
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
-  <!-- Libreria para imprimir -->
-  <script type="text/javascript" src="librerias/js/jquery.PrintArea.js"></script>
-
-
 </head>
 
 <body>
@@ -111,17 +84,6 @@ error_reporting(E_ALL);
         </div>
         <div id="selectProfesores">
           <select class="selectpicker" required multiple id="selecProfe" data-live-search="true" title="Seleccione Profesor">
-            <!--  <php    
-        $result = $ldap->getProfes();
-
-        foreach ($result as  $key) {
-          ?>
-          <option value=" <php  echo $key['apellidos'] ?> " >
-            <php  echo $key['apellidos'] ?>
-          </option> 
-          <php
-        }    
-        ?> -->
           </select>
         </div>
         <div id="seleccion"></div>
@@ -162,6 +124,27 @@ error_reporting(E_ALL);
     </footer>
   </div>
 </body>
+<!-- script  -->
+<!-- jQuery CDN versión 2.2.4 ya que bootstrap no sopeorta la 3 -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+  crossorigin="anonymous"></script>
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-86031054-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
+<!-- Libreria para imprimir -->
+<script type="text/javascript" src="librerias/js/jquery.PrintArea.js"></script>
 <script src="/ProyectoDawClaustro/js/scripts.js"></script>
 
 </html>

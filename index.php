aff369dd5fd1e0185aa8b83e6906dc6d52e392a6
Jose Luis Rego <regorodrijl@gmail.com>
2018-02-01 21:46:18 +0100

    Añadidas librerias de forma local

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
-<?php 
-error_reporting(E_ALL);
-?>
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="es">
 
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta charset="utf-8">
   <meta name="description" content="Proyecto DAW IES San Clemente. Claustro de Profesores.">
   <meta name="author" content="Jose Luis Rego Rodríguez">
   <link rel="icon" href="../../favicon.ico">
-  <link rel="stylesheet" type="text/css" href="css/ClaustroiNet.css">
+  <link rel="stylesheet" type="text/css" href="/ProyectoDawClaustro/css/ClaustroiNet.css">
   <title>ClaustroiNet</title>
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="/ProyectoDawClaustro/librerias/libsExternas/css/bootstrap.css">
   <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="/ProyectoDawClaustro/librerias/libsExternas/css/bootstrap-theme.min.css">
+  <!-- Normalize -->
+  <link rel="stylesheet" href="/ProyectoDawClaustro/librerias/libsExternas/css/normalize.css">
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="/ProyectoDawClaustro/librerias/libsExternas/css/bootstrap-select.min.css">
 </head>
 
 <body>
@@ -126,25 +126,13 @@ error_reporting(E_ALL);
 </body>
 <!-- script  -->
 <!-- jQuery CDN versión 2.2.4 ya que bootstrap no sopeorta la 3 -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript" src="/ProyectoDawClaustro/librerias/libsExternas/js/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-  crossorigin="anonymous"></script>
-<script>
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-  ga('create', 'UA-86031054-1', 'auto');
-  ga('send', 'pageview');
-</script>
+<script type="text/javascript" src="/ProyectoDawClaustro/librerias/libsExternas/js/bootstrap.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="/ProyectoDawClaustro/librerias/libsExternas/js/bootstrap-select.min.js"></script>
 <!-- Libreria para imprimir -->
-<script type="text/javascript" src="librerias/js/jquery.PrintArea.js"></script>
-<script src="/ProyectoDawClaustro/js/scripts.js"></script>
+<script type="text/javascript" src="ProyectoDawClaustro/librerias/js/jquery.PrintArea.js"></script>
+<script type="text/javascript" src="/ProyectoDawClaustro/js/scripts.js"></script>
 
 </html>

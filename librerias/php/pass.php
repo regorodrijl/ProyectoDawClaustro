b4394d256fa2d0c04a32d5f8157725e99d0d23b6
Jose Luis Rego Rodríguez <regorodrijl@gmail.com>
2018-03-15 23:31:36 +0100

    allanando el camino para el login

--- librerias/php/pass.php
+++ librerias/php/pass.php
@@ -1,9 +1,23 @@
-//getdate()
 <?php
+$host= 'localhost';
+$dbname='claustro';
+$user='root';
+$pass='';
+
+try {
+	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(PDOException $e) {
+	echo 'Falló la conexión: ' .$e->getMessage();
+}
+
+
 $opciones = [
     'cost' => 12,
 ];
-echo(getdate()["hours"]);
+
+
 $password="usuario"."password".getdate()["hours"];
 var_dump( password_hash($password, PASSWORD_BCRYPT, $opciones)."\n");
 
@@ -17,139 +31,57 @@ class Password {
     }
 }
 
+//login hacemos pass guardando hash
+$p=new Password();
+echo "\n\n\n\n";
+$p->hash($password);
+var_dump($p);
+//luego token guardamos hash con hora y fecha para token y devolvemos token, 
+var_dump($f->verify($password,$p));
 
-//Obtenemos contraseña desde un form.
-$contrasena = $_POST['contra'] ?: '';
-
-//Encriptamos de manera segura la contraseña
-$contrasena = password_hash(
-                    base64_encode(
-                        hash('sha256', $contrasena, true)
-                    ),
-                PASSWORD_DEFAULT
-            );
-
-
-//Sentencia SQL.
-$stmt = $conexion->prepare("INSERT INTO tabla (password) VALUES (?)");
-
-//Ligamos parametros marcadores.
-$stmt->bind_param("s",$contrasena);
-
-//Ejecutamos sentencia.
-$stmt->execute();
-
-//Cerramos sentencia.
-$stmt->close();
+echo(getdate()["hours"]);
+echo(getdate()["mday"]);
+echo(getdate()["mon"]);
+echo(getdate()["year"]);
+$f=new Password();
+echo "coincide";
 
 
+//en cada peticion incluir token y primero comprobar si esta ok! si no redirigir a relogin
 
 
-
-
+//$stmt = $pdo->prepare("insert into claustro(titulo,dia,horaInicio,horaFin,curso,orden,observacion,activo,borrado) values(:titulo,:dia,:horaInicio,:horaFin,:curso,:orden,:observacion,true,false)");
+// $stmt->bindParam(':titulo', $res['titulo']);
+// $stmt->bindParam(':dia', $res['dia']);
+// $stmt->bindParam(':horaInicio', $res['horaInicio']);
+// $stmt->bindParam(':horaFin', $res['horaFin']);
+// $stmt->bindParam(':curso', $res['curso']);
+// $stmt->bindParam(':orden', $res['orden']);
+// $stmt->bindParam(':observacion', $res['observacion']);
+// $stmt->execute();
 
 
 
+// //Obtenemos contraseña desde un form.
+// $contrasena = $_POST['contra'] ?: '';
+
+// //Encriptamos de manera segura la contraseña
+// $contrasena = password_hash(
+//                     base64_encode(
+//                         hash('sha256', $contrasena, true)
+//                     ),
+//                 PASSWORD_DEFAULT
+//             );
 
 
+// //Sentencia SQL.
+// $stmt = $conexion->prepare("INSERT INTO tabla (password) VALUES (?)");
 
+// //Ligamos parametros marcadores.
+// $stmt->bind_param("s",$contrasena);
 
-// // // // // // // Te voy a dejar un ejemplo como guardar de metodo segura la contraseña con password_hash(), no aplicaria un md5 solo, ya que existen páginas donde podemos descrifrar facilmente las contraseñas  md5, por ejemplo.
+// //Ejecutamos sentencia.
+// $stmt->execute();
 
-// // // // // // // http://md5cracker.org/
-
-// // // // // // // Veamos el funcionamiento password_hash(), crea un nuevo hash de contraseña usando un algoritmo de hash fuerte de único sentido. password_hash() es compatible con crypt(). Por lo tanto, los hash de contraseñas creados con crypt() se pueden usar con password_hash().
-
-//Obtenemos contraseña desde un form.
-$contrasena = $_POST['contra'] ?: '';
-
-//Encriptamos de manera segura la contraseña
-$contrasena = password_hash(
-                    base64_encode(
-                        hash('sha256', $contrasena, true)
-                    ),
-                PASSWORD_DEFAULT
-            );
-
-
-//Sentencia SQL.
-$stmt = $conexion->prepare("INSERT INTO tabla (password) VALUES (?)");
-
-//Ligamos parametros marcadores.
-$stmt->bind_param("s",$contrasena);
-
-//Ejecutamos sentencia.
-$stmt->execute();
-
-//Cerramos sentencia.
-$stmt->close();
-// // // // // Ahora vemos como podemos verificar la contraseña insertada con hash_equals.
-
-// // // // // hash_equals — Comparación de strings segura contra ataques de temporización
-
-$contrasena = $_POST['contra'] ?: '';    
-
-//Encriptamos de manera segura la contraseña
-$contrasena = password_hash(
-                    base64_encode(
-                        hash('sha256', $contrasena, true)
-                    ),
-                PASSWORD_DEFAULT
-            );
-
-//Sentencia SQL
-$stmt = $conexion->prepare("SELECT password FROM tabla WHERE password = ?");
-
-//Ligamos parametros marcadores.
-$stmt->bind_param("s",$contrasena);
-
-//Ejecutamos sentencia.
-$stmt->execute();  
-
-$stmt->store_result();
-if($stmt->num_rows===1) {
-
-    $stmt->bind_result($contrasenaBD);
-    $stmt->fetch();
-    $stmt->close();
-
-   if (hash_equals($contrasena,$contrasenaDB)) {
-      //Hacemos algo.
-
-   }        
-
-} else { $stmt->close(); }
-
-if(isset($_REQUEST["login_pass"])){$login_pass = $_REQUEST["login_pass"];}else{$login_pass = "";}
-require 'conexion.php';
-
-$login_pass = "2000000";
-
-$contrasena = password_hash(
-    base64_encode(
-        hash('sha256',$login_pass, true)
-    ),
-    PASSWORD_DEFAULT
-);
-
-//password_verify — Comprueba que la contraseña coincida con un hash, asique no debes volver a hashearla con password_hash.
-
-$sql_in="SELECT LIDER_EXPLOIT FROM LIDERES WHERE LIDER_EXPLOIT ='".$contrasena."'";
-
-$result = mysqli_query($conn,$sql_in);
-$row=mysqli_fetch_array($result);
-
-//Comprobación -> Contraseña -> OK
-if (password_verify(
-        base64_encode(
-            hash('sha256', $login_pass, true)
-        ),
-        $row["LIDER_EXPLOIT"]
-)) {
-
-  echo 'es igual';
-}
-else
-{
-  echo 'no es igual';
-}
+// //Cerramos sentencia.
+// $stmt->close();
